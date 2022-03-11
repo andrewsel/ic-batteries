d77b23d0b4bcf9c5c0e54bae3d99fa593e91c9d3
Andrew <andrew@myfuturesuper.com.au>
2022-03-11 23:14:06 +1100

    fix: bg links to prod

--- frontend/scripts/helpers.js
+++ frontend/scripts/helpers.js
@@ -1,10 +1,10 @@
 export const bgUrl = {
-  1: "http://localhost:3000/frontend/assets/stars.svg",
-  2: "http://localhost:3000/frontend/assets/stars2.svg",
-  3: "http://localhost:3000/frontend/assets/bubble.svg",
-  4: "http://localhost:3000/frontend/assets/dots.svg",
-  5: "http://localhost:3000/frontend/assets/waves.svg",
-  6: "http://localhost:3000/frontend/assets/redsky.svg",
+  1: "https://wo25k-iqaaa-aaaan-qacda-cai.raw.ic0.app/frontend/assets/stars.svg",
+  2: "https://wo25k-iqaaa-aaaan-qacda-cai.raw.ic0.app/frontend/assets/stars2.svg",
+  3: "https://wo25k-iqaaa-aaaan-qacda-cai.raw.ic0.app/frontend/assets/bubble.svg",
+  4: "https://wo25k-iqaaa-aaaan-qacda-cai.raw.ic0.app/frontend/assets/dots.svg",
+  5: "https://wo25k-iqaaa-aaaan-qacda-cai.raw.ic0.app/frontend/assets/waves.svg",
+  6: "https://wo25k-iqaaa-aaaan-qacda-cai.raw.ic0.app/frontend/assets/redsky.svg",
 }
 
 export const colors = [
